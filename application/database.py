4d0bcea789eb32abaeddf7d16c48564962ac131b
Javier Huerta <javierhuerta91@gmail.com>
2017-11-14 11:37:56 -0300

    archivos para base de datos en postgres

--- application/database.py
+++ application/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///test.db', convert_unicode=True)
+engine = create_engine(
+    'postgres://hqzywnlmociqux:864343d9c0c4884e4026c78be4819d1e8b789f3ed16f460bdf258d93ccb3d5b1@ec2-54-225-70-53.compute-1.amazonaws.com:5432/d76ur6jvtbdjt0',
+    convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
